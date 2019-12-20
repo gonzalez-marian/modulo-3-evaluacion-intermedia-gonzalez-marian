b72225141063afac40b5e52cbd4aa231a2a24194
Marian Gonzalez <gonzalezmarian170@gmail.com>
2019-12-20 16:46:10 +0100

    update docs

---
+++ docs/precache-manifest.616555047c5243085025c56f31b8f9d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "901c509a7cfbd422c6dc774ac1aa9a1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86ba39b4ef8ee9048379",
+    "url": "./static/css/main.94fc75b8.chunk.css"
+  },
+  {
+    "revision": "8369b7d2fa572ee3f464",
+    "url": "./static/js/2.6d898f53.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6d898f53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86ba39b4ef8ee9048379",
+    "url": "./static/js/main.ce35d75d.chunk.js"
+  },
+  {
+    "revision": "bb15b93d023454734eb5",
+    "url": "./static/js/runtime-main.9bff60b0.js"
+  }
+]);
